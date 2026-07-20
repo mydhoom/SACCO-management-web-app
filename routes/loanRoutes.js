b46dd8cf91d0c8597a7f4215cdf196fab84cb07e
Kapil Thakur <kapil_gtl@hotmail.com>
2026-07-20 18:46:19 +0530

    feat: add route for members to apply for a loan

--- routes/loanRoutes.js
+++ routes/loanRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const { authenticate, authorize } = require("../middlewares/authMiddleware");
-const { requestLoan, getLoans, updateLoanStatus } = require("../controllers/loanController");
+const { 
+  requestLoan, 
+  getLoans, 
+  updateLoanStatus, 
+  applyForLoan // <-- Added this here!
+} = require("../controllers/loanController");
 
 const router = express.Router();
 
 router.post("/", authenticate, authorize(["member", "admin"]), requestLoan);
 router.get("/", authenticate, authorize(["admin"]), getLoans);
 router.put("/:id", authenticate, authorize(["admin"]), updateLoanStatus);
+
+// Route for members to apply for a loan
+router.post("/apply", authenticate, applyForLoan);
 
 module.exports = router;
