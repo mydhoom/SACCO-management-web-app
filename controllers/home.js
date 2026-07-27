2c5033f5e484f63a0632fbf55004ace518a96c33
Kapil Thakur <kapil_gtl@hotmail.com>
2026-07-27 17:55:04 +0530

    refactor: clean up formatting and improve consistency in home controller methods

--- controllers/home.js
+++ controllers/home.js
@@ -3,32 +3,30 @@ const User = require('../models/User');
 const path = require('path');
 const multer = require('multer');
 
-
-
 exports.index = (req, res) => {
-res.render("index")
-}
+  res.render("index");
+};
 
 exports.deposit = (req, res) => {
   res.render("deposit");
-}
+};
 
 exports.depositmoney = (req, res) => {
   res.redirect('/depositwith');
-}
+};
 
 exports.depositwith = (req, res) => {
   res.render('depositwith');
-}
+};
 
 exports.mmoneypindeposit = (req, res) => {
-  res.render('mmoneypindeposit')
-}
+  res.render('mmoneypindeposit');
+};
 
 exports.myaccount = (req, res) => {
-  res.render('myaccount')
-}
+  res.render('myaccount');
+};
 
 exports.loanHistory = (req, res) => {
-  res.render("loanHistory")
-}
+  res.render("loanHistory");
+};
